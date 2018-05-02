39b81fb550f981d49fb293017c0723f2d1d8a9c5
Patrick Robinson <patrick.robinson@envato.com>
2018-05-02 14:08:05 +1000

    Remove define_method
    Further refactoring to simplify code

--- lib/cloudformation_rspec/matchers/output.rb
+++ lib/cloudformation_rspec/matchers/output.rb
@@ -2,43 +2,36 @@ require 'json'
 require 'yaml'
 
 RSpec::Matchers.define :have_output_including do |output_name|
-  define_method :json_template? do |template_body|
+  def json_template?(template_body)
     template_body =~ /^{/x # ignore leading whitespaces
   end
 
-  define_method :sparkle_template do |stack|
+  def compile_sparkle_template(stack)
     stack[:compile_state] ||= {}
 
-    CloudFormationRSpec::Sparkle.compile_sparkle_template(stack[:template_file], stack[:compile_state])
+    template = CloudFormationRSpec::Sparkle.compile_sparkle_template(stack[:template_file], stack[:compile_state])
+    JSON.load(template)
   end
 
-  match do |stack|
-    if stack.is_a?(Hash) && stack[:compiler] == :sparkleformation
-      if !stack[:template_file]
+  match do |input|
+    if input.is_a?(Hash) && input[:compiler] == :sparkleformation
+      if !input[:template_file]
         raise ArgumentError, "You must pass a hash to this expectation with at least the :template_file option"
       end
 
-      begin
-        template = sparkle_template(stack)
-      rescue CloudFormationRSpec::Sparkle::InvalidTemplate => error
-        raise SyntaxError, "Unable to parse SparkleFormation template #{error}"
-      end
-    elsif !stack.is_a?(String)
+      decode_function = lambda { |s| compile_sparkle_template(s) }
+    elsif !input.is_a?(String)
       raise ArgumentError, "You must pass a hash for SparkleFormation templates, or a string for YAML/JSON templates"
-    else
-      template = stack
-    end
-  
-    if json_template?(template)
+    elsif json_template?(input)
       decode_function = lambda { |tmpl| JSON.load(tmpl) }
     else
       decode_function = lambda { |tmpl| YAML.load(tmpl) }
     end
 
     begin
-      template = decode_function.call(template)
-    rescue JSON::ParserError, Psych::SyntaxError => error
-      raise SyntaxError, "Unable to parse template as either YAML or JSON. Got #{error}"
+      template = decode_function.call(input)
+    rescue JSON::ParserError, Psych::SyntaxError, CloudFormationRSpec::Sparkle::InvalidTemplate => error
+      raise SyntaxError, "Unable to parse template: #{error}"
     end
     outputs = template["Outputs"].nil? ? [] : template["Outputs"].keys
 
